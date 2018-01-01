829a2c2c6673fb6d8f647840c313ff933d4df378
ceo177 <34103930+ceo177@users.noreply.github.com>
2018-01-01 22:17:02 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/testradiobuttonquiz/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.android.testradiobuttonquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    public void sprOdpowiedź(View view) {
+        RadioGroup radioGroup_1 = (RadioGroup) findViewById(R.id.radioGr1);
+        RadioButton button2 = (RadioButton) findViewById(R.id.radioButton2);
+
+        boolean button2checked = button2.isChecked();
+
+        if (button2checked == true) {
+            Toast.makeText(getApplicationContext(), "poprawna odp!!", Toast.LENGTH_LONG).show();
+
+        } else {
+            Toast.makeText(getApplicationContext(), "zła odpowiedź!!!", Toast.LENGTH_LONG).show();
+        }
+        radioGroup_1.clearCheck();
+
+
+    }
+}
